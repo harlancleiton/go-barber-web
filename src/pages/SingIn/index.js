412ee2ab00514856dfd5721dd28aedaef93ed1a5
Harlan Cleiton <harlancleiton@gmail.com>
2019-11-05 07:28:34 -0300

    Auth styles

--- src/pages/SingIn/index.js
+++ src/pages/SingIn/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
 
 export default function SingIn() {
   return (
-    <div>
-      <h1>SingIn</h1>
-    </div>
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input type="email" placeholder="Seu email" />
+        <input type="password" placeholder="Sua senha" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta</Link>
+      </form>
+    </>
   );
 }
--- src/pages/SingUp/index.js
+++ src/pages/SingUp/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
 
 export default function SingUp() {
   return (
-    <div>
-      <h1>SingUp</h1>
-    </div>
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input type="text" placeholder="Nome completo" />
+        <input type="email" placeholder="Seu email" />
+        <input type="password" placeholder="Sua senha" />
+
+        <button type="submit">Criar Conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
   );
 }
