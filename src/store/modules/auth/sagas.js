3bf1757570a6b5bdee10718725f02d5d99c34b14
Harlan Cleiton <harlancleiton@gmail.com>
2019-11-05 13:58:12 -0300

    feat: Auth/SingIn

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,3 +1,24 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
-export default all([]);
+import api from '~/services/api';
+import { singInSuccess } from './actions';
+import history from '~/services/history';
+
+export function* singIn({ payload }) {
+  const { email, password } = payload;
+
+  const response = yield call(api.post, 'sessions', { email, password });
+
+  const { token, user } = response.data;
+
+  if (!user.provider) {
+    console.tron.error('Usuario não é prestador de serviço');
+    return;
+  }
+
+  yield put(singInSuccess(token, user));
+
+  history.push('/dashboard');
+}
+
+export default all([takeLatest('@auth/SING_IN_REQUEST', singIn)]);
