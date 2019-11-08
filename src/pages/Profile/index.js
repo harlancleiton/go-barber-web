e148d25225722555ed47376ba5fe4418417a1bed
Harlan Cleiton <harlancleiton@gmail.com>
2019-11-08 15:01:25 -0300

    feat: Add AvatarInput

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 import { Container } from './styles';
 import { updateProfileRequest } from '~/store/modules/user/actions';
+import AvatarInput from './AvatarInput';
 
 export default function Profile() {
   const dispatch = useDispatch();
@@ -16,6 +17,8 @@ export default function Profile() {
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu email" />
 
--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -5,9 +5,9 @@ import api from '~/services/api';
 import { updateProfileFailure, updateProfileSuccess } from './actions';
 
 function* updateProfile({ payload }) {
-  const { name, email, ...rest } = payload.data;
+  const { name, email, avatar_id, ...rest } = payload.data;
 
-  const profile = { name, email, ...(rest.oldPassword ? rest : {}) };
+  const profile = { name, email, avatar_id, ...(rest.oldPassword ? rest : {}) };
 
   const response = yield call(api.put, 'users', profile);
 
